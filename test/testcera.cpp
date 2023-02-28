6f41fb88f6b668e18314b8a220d329644b35e1cf
M-Try <37935522+M-Try@users.noreply.github.com>
2023-02-28 11:14:30 +0100

    WIP: updating testcera

--- test/testcera.cpp
+++ test/testcera.cpp
@@ -1,4 +1,6 @@
-#include "/src/cera_init.hpp"
+#include <stdio.h>
+#include "src/cera_vm.hpp"
+#include "src/cera_init.hpp"
 
 const unsigned char vm_guest_code[] = {
     0xba, 0xf8, 0x03, /* mov $0x3f8, %dx ; big endian number*/ 
@@ -13,23 +15,30 @@ const unsigned char vm_guest_code[] = {
     0xf4              /* hlt */
 };
 
+void io_handler(void *io_area, size_t io_size) {
+    putchar(*(char *)io_area);
+}
+
+using Ceramium;
+
+HMem_Area_Specifier mem = {&vm_guest_code, sizeof(vm_guest_code)};
 
 int main(int argc, const char **argv) {
-    Ceramium::Init();
+    Init();
 
-    Ceramium::Cera_Vm _ceratest;
+    Cera_Vm _ceratest;
 
-    _ceratest.Insert_Mem(0, 1);
-    _ceratest.Insert_Cera_Core();
-    _ceratest.Config_Cera_Core(CERA_SETCCMEM, 0, 0);
+    _ceratest.Insert_Mem(0, 1, 0);
+    _ceratest.Set_Mem(0, mem, 0);
 
-    _ceratest.Manip_Mem(0, &vm_guest_code, sizeof(vm_guest_code));
+    CCore_Id_t _core1 = _ceratest.Insert_Cera_Core();
+
 
     _ceratest.Config_Cera_Core(CERA_SETCCRIP, 0, 0x1000);
 
-    _ceratest.Run_Single_Core(0, CERA_R_IOLESS);
+    _ceratest.Run_Single_Core(_core1, 0);
 
-    _ceratest.Remove_Cera_Core(0);
+    _ceratest.Remove_Cera_Core(_core1);
     _ceratest.Remove_Mem(0);
 
     return 0;
@@ -38,39 +47,10 @@ int main(int argc, const char **argv) {
 /*
 while (1) {
     clock_t _begin_time = clock();
-
-    run_vcpu(vcpu);
-
-    switch (run->exit_reason) {
-        case KVM_EXIT_HLT:
             printf("Execution took %f seconds time\n", double(clock() - _begin_time) / CLOCKS_PER_SEC);
-            puts("KVM_EXIT_HLT");
-                return EXIT_SUCCESS;
-
         case KVM_EXIT_IO:
             if (run->io.direction == KVM_EXIT_IO_OUT &&
                 run->io.size == 1 &&
                 run->io.port == 0x3f8 &&
-                run->io.count == 1
-            ) {
-                putchar(*(((char *)run) + run->io.data_offset));
-            }
-            else {
-                errx(1, "unhandled KVM_EXIT_IO");
-            }
-            break;
-
-        case KVM_EXIT_FAIL_ENTRY:
-            logger.log(LOG_FATAL, "hardware entry failure; cannot proceed, exiting.");
-            errx(1, "KVM_EXIT_FAIL_ENTRY: hardware_entry_failure_reason = 0x%llx", (unsigned long long) run->fail_entry.hardware_entry_failure_reason);
-
-        case KVM_EXIT_INTERNAL_ERROR:
-            logger.log(LOG_FATAL, "KVM kernel internal fault; cannot proceed, exiting.");
-            errx(1, "KVM_EXIT_INTERNAL_ERROR: suberror = 0x%x",
-            run->internal.suberror);
-
-        default:
-            err(EXIT_FAILURE, "what.");
-    }
-}
+                run->io.count == 1)
 */
